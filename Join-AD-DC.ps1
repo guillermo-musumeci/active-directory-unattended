2ede49ac526939ac91b34ea5ee2a013780e8bb3e
Guillermo Musumeci <guillermo-musumeci@users.noreply.github.com>
2019-09-09 12:25:00 +0200

    Initial commit

---
+++ Join-AD-DC.ps1
@@ -0,0 +1,38 @@
+# Variables
+$DomainName = "kopicloud.local"
+$DomainNetbiosName = "KOPICLOUD"
+$DatabasePath = "C:\Windows\NTDS"
+$SysvolPath = "C:\Windows\SYSVOL"
+$LogPath = "C:\Windows\NTDS"
+$SafeModeAdministratorPassword = "Ch@ng3M3"
+$ADAdmin = "kopiadmin"
+$ADPassword = "Ch@ng3M3"
+
+# Import ServerManager 
+Import-Module ServerManager 
+ 
+# Install AD & DNS Features
+Install-WindowsFeature AD-Domain-Services -IncludeAllSubFeature -IncludeManagementTools 
+Install-WindowsFeature DNS -IncludeAllSubFeature -IncludeManagementTools 
+
+# Import Management Modules
+Import-Module ADDSDeployment 
+Import-Module DnsServer 
+
+# Credentials
+$User = $DomainNetbiosName + "\" + $ADAdmin
+$PWord = ConvertTo-SecureString -String $ADPassword -AsPlainText -Force
+$Credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $User, $PWord
+ 
+# Install and Configure AD
+Install-ADDSDomainController `
+-DomainName $DomainName `
+-Credential $Credential `
+-InstallDns:$true `
+-DatabasePath $DatabasePath `
+-SysvolPath $SysvolPath `
+-LogPath $LogPath `
+-Force:$true `
+-NoRebootOnCompletion:$false `
+-SafeModeAdministratorPassword `
+    (ConvertTo-SecureString $SafeModeAdministratorPassword -AsPlainText -Force)
